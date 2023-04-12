4290be25c69550b12ea0f6754f00f1bdb98796ab
Max Axanii <maxaxanii@Maxs-MacBook-Air.local>
2023-04-12 10:09:07 +0300

    First commit

---
+++ src/ImageRenderFunction.jsx
@@ -0,0 +1,21 @@
+import { arrayForList } from "./arrayForList";
+
+
+function Image({ src, name }) {
+   return <img src={src} alt={name} />;
+ }
+ 
+ function ImageList({ images }) {
+   return (
+     <div>
+       {images.map((image) => (
+         <Image key={image.id} src={image.src} name={image.name} style='w-2' />
+       ))}
+     </div>
+   );
+ }
+ 
+ 
+ export function Appl() {
+   return <ImageList images={arrayForList} />;
+ }
